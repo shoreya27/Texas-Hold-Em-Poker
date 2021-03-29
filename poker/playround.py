eefb252a6430844f91f2cbf9688dc0a826f2b1c7
shoreya27 <gupta.shoreya2707@gmail.com>
2021-03-29 13:22:41 +0530

    playground can remove cards from deck now

--- poker/playround.py
+++ poker/playround.py
@@ -6,3 +6,8 @@ class PlayGround():
     
     def play(self):
         self.deck.shuffle()
+
+        #now remove 2 cards for each player present
+        #in game
+        for player in self.players:
+            self.deck.remove_cards(2)
--- test/test_playround.py
+++ test/test_playround.py
@@ -1,7 +1,7 @@
 import unittest
 from poker.deck import Deck
 from poker.player import Player
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, call
 from poker.playround import PlayGround
 
 class TestPlayround(unittest.TestCase):
@@ -41,3 +41,28 @@ class TestPlayround(unittest.TestCase):
 
         play_round.play()
         deck_mock.shuffle.assert_called_once()
+    
+    def test_to_check_initial_2_cards_removed_for_each_player(self):
+        '''
+        I have 3 players , each player will get 2 cards
+        total 3 times remove_card() of deck should
+        be called
+        special method in mock object to check
+        the total calls happened for this funct
+        each call(arg) represent 1 call to that
+        remove_card function 
+        '''
+        deck_mock = MagicMock()
+        players = [
+            MagicMock(), 
+            MagicMock(),
+            MagicMock()
+        ]
+        
+        play_round = PlayGround(deck = deck_mock,player = players)
+        play_round.play()
+        deck_mock.remove_cards.assert_has_calls(
+            [call(2), 
+            call(2),
+            call(2)]
+        )
